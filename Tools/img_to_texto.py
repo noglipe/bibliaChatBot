5be58366ed56fa6a44a523f381b750021070d3fc
Filipe Nogueira <nog.lipe@gmail.com>
2022-04-15 18:01:54 -0300

    UPDATE - Funcoes Extras

--- Tools/img_to_texto.py
+++ Tools/img_to_texto.py
@@ -9,8 +9,8 @@ def converter(id):
     imagem = cv2.imread(f'img\{id}.jpg')
     print(f'{Tools.tools.time()}-{id} - Imagem Lida!')
 
-    caminho = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-    pytesseract.pytesseract.tesseract_cmd = caminho
+    #caminho = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    #pytesseract.pytesseract.tesseract_cmd = caminho
 
     texto = pytesseract.image_to_string(imagem, lang="por")
     print(f'{Tools.tools.time()}-{id} - Texto Enviado!')
